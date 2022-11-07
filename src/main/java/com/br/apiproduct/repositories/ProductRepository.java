e5d13952da3b81891e3f5f4a617d93caf6397590
Joel Maciel <soldado25461@gmail.com>
2022-11-06 21:47:41 -0300

    feat: creating the folder architecture and implementing the ProductService

---
+++ src/main/java/com/br/apiproduct/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.br.apiproduct.repositories;
+
+import com.br.apiproduct.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
